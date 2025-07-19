4cfc5fea0aa925d355ac41eedca2e535cb49eab2
Dilkhush saini <sainidilkhush019@gmail.com>
2025-07-19 21:49:22 +0530

    some modification

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,20 +1,25 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const adminController = require("../controllers/adminController");
+const upload = require("../middleware/multer");
 
 router.post("/reg", userController.regController);
 router.post("/login", userController.loginController);
 router.get("/show-trending", userController.showTrendingController);
 router.post("/userquery", userController.userQueryController);
 
-router.post("/create-product", adminController.createProduct);
+router.post(
+  "/create-product",
+  upload.single("image"),
+  adminController.createProduct
+);
 router.get("/show-product", adminController.showProduct);
 router.get("/getsingleproduct/:id", adminController.getsingleproduct);
 router.delete("/delete-product/:id", adminController.deleteProduct);
 router.put("/update-product", adminController.updateProduct);
-router.get("/show-query",adminController.showQueryController)
-router.delete("/delete-query",adminController.deleteQueryController)
-router.get("/single-query/:id",adminController.getSingleQuery)
-router.post("/reply-query",adminController.replyQueryController)
+router.get("/show-query", adminController.showQueryController);
+router.delete("/delete-query", adminController.deleteQueryController);
+router.get("/single-query/:id", adminController.getSingleQuery);
+router.post("/reply-query", adminController.replyQueryController);
 
 module.exports = router;
